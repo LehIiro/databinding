3c09d2a7769b47f50cba3d65b1783e012b4a90cd
LehIiro <34263385+LehIiro@users.noreply.github.com>
2018-01-16 12:15:55 +0200

    Revert "Revert "Revert "readme.txt"""
    This reverts commit fc2780a071af8aaa685a786ec280d4ae9c416053.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { Binder } from './binder.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Binder
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
